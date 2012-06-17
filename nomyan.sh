6dea27947ead328054fddccf9e41a0f0899067c5
moepi <martin.dahse@gmail.com>
2012-06-17 02:25:56 +0200

    added multiple API keys support

--- nomyan.sh
+++ nomyan.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #################################
-### Please enter your API key in
-### a file with APIKEY=<key> in
+### Please enter your API keys in
+### a file with
+### APIKEYS="<key> [key]*"
+### in follow files or options
+### 
 ### /etc/nomyan.key
 ### ~/.nomyan.key
 ### or as option -k
@@ -26,7 +29,7 @@ usage: $filename application event description
 This script notifies your android devices via Notify-My-Android app.
 
 OPTIONS:
-	-k	Specify the API-Key (overrides from apikey-files)
+	-k	Specify the API keys (overrides API keyfiles)
 	-l	Specify logfile
 	-L	Disable Logging to file
 	-v	Verbose output
@@ -65,7 +68,7 @@ done
 
 shift $((OPTIND-1))
 
-[[ -z $APIKEY ]] && logger "No API-Key specified." && logger "Please create API keyfile or use -k option" && usage
+[[ -z $APIKEYS ]] && logger "No API keys specified." && logger "Please create API keyfiles or use -k option" && usage
 NOTIFYURL="https://nma.usk.bz/publicapi/notify"
 CURL="`which curl`"
 [[ -z $CURL ]] && logger "curl not installed" && exit 1
@@ -73,28 +76,28 @@ CURL="`which curl`"
 #check for right number of arguments
 [[ ! $# -eq 3 ]] && logger "Wrong number of arguments." && usage
 
-NOTIFY="`curl -s --data-ascii "apikey=$APIKEY" --data-ascii "application=$1" --data-ascii "event=$2" --data-asci "description=$3" $NOTIFYURL -o- | sed 's/.*success code="\([0-9]*\)".*/\1/'`"
-case $NOTIFY in
-	200)
-	logger "Notification submitted."
-	exit 0
-	;;
-	400)
-	logger "The data supplied is in the wrong format, invalid length or null."
-	exit 400
-	;;
-	401)
-	logger "None of the API keys provided were valid."
-	exit 401
-	;;
-	402)
-	logger "Maximum number of API calls per hour exceeded."
-	exit 402
-	;;
-	500)
-	logger "Internal server error. Please contact our support if the problem persists."
-	exit 500
-	;;
-esac
-logger "something wen't badly wrong here!"
-exit 9001
+for d in $APIKEYS; do
+	NOTIFY="`curl -s --data-ascii "apikey=$d" --data-ascii "application=$1" --data-ascii "event=$2" --data-asci "description=$3" $NOTIFYURL -o- | sed 's/.*success code="\([0-9]*\)".*/\1/'`"
+	case $NOTIFY in
+		200)
+		logger "Notification submitted."
+		;;
+		400)
+		logger "The data supplied is in the wrong format, invalid length or null."
+		exit 400
+		;;
+		401)
+		logger "None of the API keys provided were valid."
+		exit 401
+		;;
+		402)
+		logger "Maximum number of API calls per hour exceeded."
+		exit 402
+		;;
+		500)
+		logger "Internal server error. Please contact our support if the problem persists."
+		exit 500
+		;;
+	esac
+done
+exit 0
