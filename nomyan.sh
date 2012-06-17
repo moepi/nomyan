d308fc845b6cf98b1029cadbc09c93882df0663f
moepi <martin.dahse@gmail.com>
2012-06-17 02:58:14 +0200

    some cleanup and added comments

--- nomyan.sh
+++ nomyan.sh
@@ -10,18 +10,23 @@
 ### or as option -k
 #################################
 
+# a simple logger function
+function logger {
+LOGLINE="`date +%Y-%m-%d_%H:%M:%S` [$$] $@"
+[[ $LOGGING -eq 1 ]] && echo -e $LOGLINE >> $LOGFILE
+[[ $VERBOSE -eq 1 ]] && echo -e $LOGLINE
+}
+
+# some default settings
 filename=$(basename "$0")
 basename=${filename%.*}
 LOGFILE="$basename.log"
 VERBOSE=0
 LOGGING=1
 PRIORITY=0
-
-function logger {
-LOGLINE="`date +%Y-%m-%d_%H:%M:%S` [$$] $@"
-[[ $LOGGING -eq 1 ]] && echo -e $LOGLINE >> $LOGFILE
-[[ $VERBOSE -eq 1 ]] && echo -e $LOGLINE
-}
+NOTIFYURL="https://nma.usk.bz/publicapi/notify"
+CURL="`which curl`"
+[[ -z $CURL ]] && logger "curl not installed" && exit 1
 
 function usage {
 cat << EOF
@@ -39,6 +44,9 @@ EOF
 exit 3
 }
 
+# check default pathes for keyfiles
+# keyfile in homedir will override keyfile in /etc/
+# keyfile specified in -k option overrides everything
 [[ -r /etc/$basename.key ]] && . /etc/$basename.key
 [[ -r ~/.$basename.key ]] && . ~/.$basename.key
 
@@ -46,46 +54,55 @@ while getopts "hk:vl:Lp:" OPTION
 do
 	case $OPTION in
 	h)
+		# print the help (usage)
 		usage
 		exit 1
 		;;
 	k)
+		# set apikey from option
 		APIKEY=$OPTARG
 		;;
 	v)
+		# enable verbose logging
 		VERBOSE=1
 		;;
 	L)
+		# disable logging to file
 		LOGGING=0
 		;;
 	l)
+		# specify logfile
 		LOGFILE=$OPTARG
 		;;
 	p)
+		# set priority
 		PRIORITY=$OPTARG
 		;;
 	?)
+		# print usage on unknown command
 		usage
 		exit
 		;;
 	esac
 done
 
+# shift parsed options for easy using $1,$2 and $3
 shift $((OPTIND-1))
 
+# check if API keys are set, if not print usage
 [[ -z $APIKEYS ]] && logger "No API keys specified." && logger "Please create API keyfiles or use -k option" && usage
-NOTIFYURL="https://nma.usk.bz/publicapi/notify"
-CURL="`which curl`"
-[[ -z $CURL ]] && logger "curl not installed" && exit 1
 
-#check for right number of arguments
+# check for right number of arguments
 [[ ! $# -eq 3 ]] && logger "Wrong number of arguments." && usage
 
+# iterate over API keylist
 for d in $APIKEYS; do
-	NOTIFY="`curl -s --data-ascii "apikey=$d" --data-ascii "application=$1" --data-ascii "event=$2" --data-asci "description=$3" --data-ascii "priority=$PRIORITY" $NOTIFYURL -o- | sed 's/.*success code="\([0-9]*\)".*/\1/'`"
+	# send notifcation
+	NOTIFY="`$CURL -s --data-ascii "apikey=$d" --data-ascii "application=$1" --data-ascii "event=$2" --data-asci "description=$3" --data-ascii "priority=$PRIORITY" $NOTIFYURL -o- | sed 's/.*success code="\([0-9]*\)".*/\1/'`"
+	# handle return code
 	case $NOTIFY in
 		200)
-		logger "Notification submitted."
+		logger "Notification submitted to API key $d."
 		;;
 		400)
 		logger "The data supplied is in the wrong format, invalid length or null."
